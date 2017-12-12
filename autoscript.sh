a4ca5f2e0a53eceaccd7a7352f98e07a025ea6c9
Bhavesh Sali <34472746+techtoniksyt@users.noreply.github.com>
2017-12-12 15:12:32 +0530

    Add files via upload

---
+++ autoscript.sh
@@ -0,0 +1,50 @@
+ #!/data/data/com.termux/files/usr/bin/bash
+
+clear
+echo "update in progress"
+sleep 2
+apt update && apt upgrade
+echo "update finished"
+sleep 2
+echo "Initiatizing...."
+sleep 1
+echo "installing GIT"
+sleep 1
+apt install git
+echo "finished"
+sleep 1
+echo "installing  NANO"
+sleep 1
+apt install nano
+echo "finished"
+sleep 1
+echo "installing WGET"
+sleep 1
+apt install wget
+echo "finished"
+sleep 1
+echo "installing PYTHON"
+sleep 1
+apt install python
+echo "finished"
+sleep 1
+echo "installing PYTHON2"
+sleep 1
+apt install python2
+echo "finished"
+sleep 1
+echo "insstalling FIGLET"
+sleep 1
+apt install figlet
+echo "finished"
+sleep 1
+echo "installing TOILET"
+sleep 1
+apt install toilet
+echo "finished"
+sleep 1
+toilet -f mono12 -F gay "SUBSCRIBE"
+toilet -f mono12 -F gay "TO"
+toilet -f mono12 -F gay "TECHTONIKS"
+sleep 5
+clear
